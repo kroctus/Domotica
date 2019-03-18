70afe334c9ca2a3fdbb6827bb4a000c4c8cf4f2f
gabriel_payano <gabriel21kun@gmail.com>
2019-03-18 20:09:56 +0100

    "Instanciación de la centralita en el main"

--- src/vista/Domotica.java
+++ src/vista/Domotica.java
@@ -5,6 +5,8 @@
  */
 package vista;
 
+import java.time.LocalDate;
+
 /**
  *
  * @author gabriel
@@ -13,12 +15,25 @@ public class Domotica {
 
     public static void main(String[] args) {
 
+        /*En el main creamos cada uno de los objetos necesarios para la instanciación de la centralita
+            Este proceso puede hacerse también directamente pasando estas instanciaciones al constructor de la centralita
+            pero de cara a una mejor lectura hemos decidido realizarlo c/u por separado*/
+        
         Garaje g1 = new Garaje();
         System.out.println(g1);
-
+        //Garajes
         Garaje g2 = new Garaje(2, new Puerta(), 50);
         System.out.println(g2);
+        //USUARIO
+        Usuario user = new Usuario("GabrielPayano", "12345gp"); 
+        //SALON
+        Salon salon1= new Salon();
+        //DORMITORIO
+        Dormitorio dormitorio1= new Dormitorio();
+        //RELOJ
+        Reloj relojAux= new Reloj();
         
+        Centralita mainCentral = new Centralita("centralitaPrincipal",LocalDate.now(),relojAux,user,g2,salon1,dormitorio1);
         
 
     }
