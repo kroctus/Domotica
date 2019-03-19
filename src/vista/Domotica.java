ae77ad0e14368122f7f5a659102f1e91ef318e9d
GabrielPayano <gabriel21kun@gmail.com>
2019-03-19 14:47:14 +0100

    "Implementamos el método menu() en el main"

--- src/vista/Domotica.java
+++ src/vista/Domotica.java
@@ -18,23 +18,23 @@ public class Domotica {
         /*En el main creamos cada uno de los objetos necesarios para la instanciación de la centralita
             Este proceso puede hacerse también directamente pasando estas instanciaciones al constructor de la centralita
             pero de cara a una mejor lectura hemos decidido realizarlo c/u por separado*/
-        
         Garaje g1 = new Garaje();
 //        System.out.println(g1);
         //Garajes
         Garaje g2 = new Garaje(2, new Puerta(), 50);
 //        System.out.println(g2);
         //USUARIO
-        Usuario user = new Usuario("GabrielPayano", "12345gp"); 
+        Usuario user = new Usuario("GabrielPayano", "12345gp");
         //SALON
-        Salon salon1= new Salon();
+        Salon salon1 = new Salon();
         //DORMITORIO
-        Dormitorio dormitorio1= new Dormitorio();
+        Dormitorio dormitorio1 = new Dormitorio();
         //RELOJ
-        Reloj relojAux= new Reloj();
-        
-        Centralita mainCentral = new Centralita("centralitaPrincipal",LocalDate.now(),relojAux,user,g2,salon1,dormitorio1);
+        Reloj relojAux = new Reloj();
+
+        Centralita mainCentral = new Centralita("centralitaPrincipal", LocalDate.now(), relojAux, user, g2, salon1, dormitorio1);
         Vista.login(mainCentral);
+        Vista.menu();
 
     }
 
