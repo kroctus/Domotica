1c25142ca334a4680ea2e89391bd85b31ecd55ee
gabriel_payano <gabriel21kun@gmail.com>
2019-03-24 18:43:49 +0100

    "Añadimos los métodos encender() y apagarLuces()"

--- src/vista/Luz.java
+++ src/vista/Luz.java
@@ -5,22 +5,56 @@
  */
 package vista;
 
+import java.util.Scanner;
+
 /**
  *
  * @author gabriel
  */
 public class Luz {
-    
+
     private boolean estado;
     private double consumo;
-    
-    //Constructor
 
+    //Constructor
     public Luz(boolean estado, double consumo) {
         this.estado = estado;
         this.consumo = consumo;
     }
-    
+
     //Método para apagar y encender las luces
-    
+    public void apagarLuces() {
+        Scanner teclado = new Scanner(System.in);
+        String opcion = "";
+        if (estado == false) {
+            System.out.println("Las luces ya estan apagadas, ¿desea encenderlas? s/n");
+            opcion = teclado.nextLine();
+            if (opcion.equalsIgnoreCase("s")) {
+
+            } else {
+                this.estado = false;
+            }
+
+        } else {
+            estado = false;
+        }
+
+    }
+
+    public void encenderLuces() {
+        Scanner teclado = new Scanner(System.in);
+        String opcion = "";
+        if (estado == true) {
+            System.out.println("Las luces ya estan encendidas, ¿desea apagarlas? s/n");
+            opcion = teclado.nextLine();
+            if (opcion.equalsIgnoreCase("s")) {
+                apagarLuces();
+            } else {
+                this.estado = true;
+            }
+
+        } else {
+            estado = true;
+        }
+    }
 }
